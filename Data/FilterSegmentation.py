83a29052628da4e72d0c2099bb7f423a74744e3c
cinnmnm <cinnmnm@gmail.com>
2025-06-13 00:24:56 +0200

    Changed FilterSegmentation.

--- Data/FilterSegmentation.py
+++ Data/FilterSegmentation.py
@@ -14,11 +14,14 @@ class FilterSegmentation:
         gray = ImagePreprocessing.grayscale(image)
         clahe = ImagePreprocessing.apply_clahe(gray)
         median_filtered = ImagePreprocessing.median_filter(clahe)
-        frangi_result = FrangiFilter2D.apply(median_filtered)
+        normalized = ImagePreprocessing.normalize(median_filtered)
+        resized_normalized = ImagePreprocessing.resize_and_normalize(normalized)
+        frangi_result = FrangiFilter2D.apply(resized_normalized)
         binary = ImagePostprocessing.adaptive_binarization(frangi_result)
         opened = ImagePostprocessing.morphological_opening(binary)
         closed = ImagePostprocessing.morphological_closing(opened)
 
+        negative = 255 - closed
         #print("Final closed type:", type(closed), "shape:", getattr(closed, "shape", None))
 
-        return closed
+        return negative
